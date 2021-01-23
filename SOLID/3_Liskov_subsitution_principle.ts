174f0d8f3b6f6cc5b71750fe1aad582154d6c11b
Mikhail <firecrafter62@gmail.com>
2021-01-23 14:35:20 +0200

    LSP

---
+++ SOLID/3_Liskov_subsitution_principle.ts
@@ -0,0 +1,24 @@
+
+// Objects should be replaceable with instances of their subclasses without altering the behavior
+
+class Bird {}
+
+class FlyingBird extends Bird {
+  fly() {
+    console.log('Fyling...');
+  }
+}
+
+class Eagle extends FlyingBird {
+  dive() {
+    console.log('Diving...');
+  }
+}
+
+const eagle = new Eagle();
+eagle.fly();
+eagle.dive();
+
+class Penguin extends Bird {
+  // Problem: Can't fly!
+}
