a982e26f223b372c6ca6d78fc8321984ea693d38
Mikhail <firecrafter62@gmail.com>
2021-01-23 14:25:24 +0200

    OCP

---
+++ SOLID/2_Open_close_priciple.ts
@@ -0,0 +1,46 @@
+// class Printer {
+//   printPDF(data: any) {
+//     // ...
+//   }
+
+//   printWebDocument(data: any) {
+//     // ...
+//   }
+
+//   printPage(data: any) {
+//     // ...
+//   }
+
+//   verifyData(data: any) {
+//     // ...
+//   }
+// }
+
+// Open for extension but closed for modification
+
+
+interface Printer {
+  print(data: any);
+}
+
+class PrinterImplementation {
+  verifyData(data: any) {}
+}
+
+class WebPrinter extends PrinterImplementation implements Printer {
+  print(data: any) {
+    // print web document
+  }
+}
+
+class PDFPrinter extends PrinterImplementation implements Printer {
+  print(data: any) {
+    // print PDF document
+  }
+}
+
+class PagePrinter extends PrinterImplementation implements Printer {
+  print(data: any) {
+    // print real page
+  }
+}
