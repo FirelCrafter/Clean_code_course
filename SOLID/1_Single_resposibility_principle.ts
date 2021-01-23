b8e5af10e6469f3a74a78607bb104b72e2ad9675
Mikhail <firecrafter62@gmail.com>
2021-01-23 11:59:37 +0200

    SRP

---
+++ SOLID/1_Single_resposibility_principle.ts
@@ -0,0 +1,15 @@
+// Not violating SRP
+class User {
+  login(email: string, password: string) {}
+
+  signup(email: string, password: string) {}
+
+  assignRole(role: any) {}
+}
+
+// Violating SRP
+class ReportDocument {
+  generateReport(data: any) {}
+
+  createPDF(report: any) {}
+}
